f1a91c30d7bd4ec38721f8231e857491b174f39f
favorable-device-5158 <blee2125@gmail.com>
2019-09-10 15:07:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,9 +9,13 @@ class Dog
   def self.all
     @@all
   end
+  
   def self.print_all
-    puts @@all
+    @@all.each do |name|
+      puts name
+    end
   end
+
   def self.clear_all
     @@all.clear
   end
